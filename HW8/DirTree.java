642ff8e6f7f56f0c0663e4cd6582716570e6c594
linmaogeng <363577572@qq.com>
2018-04-29 22:56:17 -0400

    Add files via upload

---
+++ HW8/DirTree.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class DirTree {
+	static String lineBreak = System.lineSeparator();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String defaultDir = "The url is invalid, use working directory.";
+		File path;
+		
+		if(args.length == 0) {
+			System.out.println(defaultDir);
+			path = new File(System.getProperty("user.dir"));
+		}
+		else {
+			path = new File(args[0]);
+		}
+		
+		if(path.isFile()) {
+			path = new File(path.getAbsolutePath());
+			path = new File(path.getParent());
+		}
+		else if(!path.isDirectory()) {
+			System.out.println(defaultDir);
+			path = new File(System.getProperty("user.dir"));
+		}		
+		printFolder(path);
+		
+	}
+	
+	public static void printFolder(File path) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(path.getName());
+		sb.append(":" + lineBreak);
+		printFolder(path,sb,new StringBuilder());
+		
+		try {
+			PrintWriter pw = new PrintWriter(new File("DirTree.txt"));
+			pw.print(sb);
+			pw.close();
+		}
+		catch(FileNotFoundException ex) {
+			System.out.println("File system ");
+			System.out.println(sb);
+		}
+	}
+	
+	private static void printFolder(File path,StringBuilder sb, StringBuilder indent) {
+		Queue<File> folder = new LinkedList<>();
+		Queue<File> file = new LinkedList<>();
+		StringBuilder fileIn = indent;
+		File subFolder;
+		try {
+			for(File name: path.listFiles()) {
+				if(name.isFile()) 
+					file.add(name);
+				
+				else 
+					folder.add(name);
+			}
+		}
+		catch(NullPointerException ex) {
+			sb.append(fileIn + "'FOLDER CAN'T BE ACCESSED!'" + lineBreak);
+		}
+		
+		if(folder.size()>0)
+			fileIn = new StringBuilder(indent).append("©¦  ");
+		
+		while(!file.isEmpty()) {
+			sb.append(fileIn + file.poll().getName() + lineBreak);
+		}
+		
+		while(!folder.isEmpty()) {
+			subFolder = folder.poll();
+			sb.append(fileIn + lineBreak + indent);
+			
+			if(folder.size()==0) {
+				sb.append("©¸©¤©¤©¤©¤©¤" + subFolder.getName() + ":" + lineBreak);
+				printFolder(subFolder,sb,new StringBuilder(indent).append("   \t"));
+			}
+			
+			else {
+				sb.append("©À©¤©¤©¤©¤©¤" + subFolder.getName() + ":" + lineBreak);
+				printFolder(subFolder,sb,new StringBuilder(fileIn).append("   \t"));
+			}
+		}
+
+	}
+
+}
+	
+
